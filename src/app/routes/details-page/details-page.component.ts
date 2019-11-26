07d1830535b309ea5a3c84e63e0dda08b20ffde5
anton-varenyuk <avarenyuk@corevalue.net>
2019-11-26 20:17:03 +0200

    edit-page: enabled editing

--- src/app/routes/details-page/details-page.component.ts
+++ src/app/routes/details-page/details-page.component.ts
@@ -3,15 +3,15 @@ import { ActivatedRoute } from '@angular/router';
 import { TaskService } from '../../services/task.service';
 import { ITask } from '../../interfaces/ITask';
 
-
 @Component({
   selector: 'app-list-item-detail',
   templateUrl: './details-page.component.html',
   styleUrls: ['./details-page.component.scss']
 })
 export class DetailsPageComponent implements OnInit {
-  pageTitle = 'Task details';
-  task: ITask;
+  private pageTitle = 'Task details';
+  private task: ITask;
+  private editEnabled: boolean = false;
 
   constructor( private taskService: TaskService,
                private route: ActivatedRoute) { }
@@ -20,5 +20,12 @@ export class DetailsPageComponent implements OnInit {
     const id = +this.route.snapshot.paramMap.get('id');
     this.task = this.taskService.getTask(id);
     console.log(this.task);
+  }
+
+  edit(): void {
+    this.editEnabled = true;
+  }
+  save(): void {
+    this.editEnabled = false;
   }
 }
