94c2b081799ff8858b885d57b25f87da0757a351
anton-varenyuk <anton.varenyuk@avenga.com>
2020-02-13 18:28:17 +0200

    step_7: email update functionality improved

---
+++ src/app/interfaces/IMyAccOptions.ts
@@ -0,0 +1,7 @@
+export interface IMyAccOptions {
+  userInfoEditable?: boolean;
+  userPasswordEditable?: boolean;
+  passwordIsUpdatedRecently?: boolean;
+  userEmailEditable?: boolean;
+  emailUpdatedRecently?: boolean;
+}
